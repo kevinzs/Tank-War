8f561a0f0334401766a749ca199897e2918b9b6f
Kevin Zarzycki <kezarszy@inf.upv.es>
2016-09-10 18:44:50 +0200

    first commit

---
+++ desktop/src/com/kezarszy/tankwar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kezarszy.tankwar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kezarszy.tankwar.TankWar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TankWar(), config);
+	}
+}
